01e802380c63b10ddd1fe65de5ea304d49122586
akhileshsb25 <akhileshsb265@gmail.com>
2025-03-11 12:21:21 +0530

    Update Main.java

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,17 @@
 package com;
 
+import com.model.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        // Create a new Book object
+        Book book = new Book();
+        
+        // Set the title and author of the book
+        book.setTitle("Complete Java");
+        book.setAuthor("Herbert Schildt");
+        
+        // Print the Book object to the console (calls the toString method)
+        System.out.println(book);  // Output: Book [title=Complete Java, author=Herbert Schildt]
     }
 }
