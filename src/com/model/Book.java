e7e66d448bf2527e7127f798d743b016d05e925f
akhileshsb25 <akhileshsb265@gmail.com>
2025-03-11 12:22:02 +0530

    Update Book.java

--- src/com/model/Book.java
+++ src/com/model/Book.java
@@ -1,33 +1,30 @@
-package com.model;  // Ensure this matches your actual package name
+package com.model;
 
 public class Book {
     private String title;
     private String author;
 
-    // Constructor
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    // Getters and Setters
+    // Getter for title
     public String getTitle() {
         return title;
     }
 
+    // Setter for title
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // Getter for author
     public String getAuthor() {
         return author;
     }
 
+    // Setter for author
     public void setAuthor(String author) {
         this.author = author;
     }
 
-    // Override toString() to print book details
+    // toString() method to provide string representation of the object
     @Override
     public String toString() {
         return "Book [title=" + title + ", author=" + author + "]";
